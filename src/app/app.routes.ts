d6de777ef0818d8c12d24c3f606b7937f6a81f31
KevinJp21 <kevinjp821@gmail.com>
2024-07-22 11:18:48 -0500

    crear componente login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import LoginComponent from './auth/login/login.component';
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: "full"
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
